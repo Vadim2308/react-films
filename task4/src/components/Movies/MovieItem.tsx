3d7b7aaa822e998f97cdde8ee0bdac052e858c0f
Vadim Niziev <st1mpro23@yandex.ru>
2021-05-20 18:49:56 +0300

    сделал анимацию карточек

--- task4/src/components/Movies/MovieItem.tsx
+++ task4/src/components/Movies/MovieItem.tsx
@@ -7,6 +7,7 @@ type TProps = {
   title: string;
   backdrop_path: string;
   poster_path: string;
+  vote_average: string;
 };
 
 export default class MovieItem extends React.Component<{ movie: TProps }> {
@@ -14,15 +15,15 @@ export default class MovieItem extends React.Component<{ movie: TProps }> {
     super(props);
   }
   render() {
-    const { title, backdrop_path, poster_path } = this.props.movie;
-    console.log(this.props.movie.backdrop_path);
+    const { title, backdrop_path, poster_path, vote_average } = this.props.movie;
+    console.log(this.props.movie);
     return (
       <div className={classes.movie}>
         <div className={classes.movie__inner}>
           <img
             className={classes.img}
             src={`https://image.tmdb.org/t/p/w500${poster_path || backdrop_path}`}></img>
-          <h4 className={classes.title}>{title}</h4>
+          <h2 className={classes.title}>{title}</h2>
         </div>
       </div>
     );
