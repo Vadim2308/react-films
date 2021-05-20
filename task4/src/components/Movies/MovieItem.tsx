83938444db6d00ad5233b10f80c70532250cf7ec
Vadim Niziev <st1mpro23@yandex.ru>
2021-05-20 19:52:13 +0300

    доделан hover при наведении

--- task4/src/components/Movies/MovieItem.tsx
+++ task4/src/components/Movies/MovieItem.tsx
@@ -8,6 +8,7 @@ type TProps = {
   backdrop_path: string;
   poster_path: string;
   vote_average: string;
+  release_date: string;
 };
 
 export default class MovieItem extends React.Component<{ movie: TProps }> {
@@ -15,7 +16,7 @@ export default class MovieItem extends React.Component<{ movie: TProps }> {
     super(props);
   }
   render() {
-    const { title, backdrop_path, poster_path, vote_average } = this.props.movie;
+    const { title, backdrop_path, poster_path, vote_average, release_date } = this.props.movie;
     console.log(this.props.movie);
     return (
       <div className={classes.movie}>
@@ -24,6 +25,10 @@ export default class MovieItem extends React.Component<{ movie: TProps }> {
             className={classes.img}
             src={`https://image.tmdb.org/t/p/w500${poster_path || backdrop_path}`}></img>
           <h2 className={classes.title}>{title}</h2>
+          <div className={classes.active}>
+            <h5 className={classes.active__rating}>{vote_average}</h5>
+            <h5 className={classes.active__release}>{release_date}</h5>
+          </div>
         </div>
       </div>
     );
