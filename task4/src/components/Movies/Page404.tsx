b2c70db02b656ed103cfe5d8d25a7c275a7f6a13
Vadim Niziev <st1mpro23@yandex.ru>
2021-05-27 00:39:09 +0300

    добавлена проверка и timeout к 404 странице

--- task4/src/components/Movies/Page404.tsx
+++ task4/src/components/Movies/Page404.tsx
@@ -1,13 +1,37 @@
 import classes from '../../styles/movies.module.scss';
+import React from 'react';
 
-const Page404 = () => {
-  return (
-    <div className={classes.error}>
-      <h1 className={classes.error__title}>
-        УПС :( Ничего не найдено...попробуйте изменить настройки фильтра и повторите снова.
-      </h1>
-    </div>
-  );
+type TState = {
+  loading: boolean;
 };
+
+class Page404 extends React.Component<{}, TState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: true });
+    }, 1000);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <div className={classes.error}>
+          <h1 className={classes.error__title}>
+            УПС :( Ничего не найдено...попробуйте изменить настройки фильтра и повторите снова.
+          </h1>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
 
 export default Page404;
