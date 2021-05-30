57ce1bf1207017e28873e25d700cf45f050c02ff
Vadim Niziev <st1mpro23@yandex.ru>
2021-05-30 20:22:58 +0300

    сделан слайдер с анимацией

--- task4/src/components/Slider/Slider.tsx
+++ task4/src/components/Slider/Slider.tsx
@@ -1,12 +1,42 @@
 import { useState } from 'react';
 import SliderItem from './SliderItem';
+import { SliderData } from './SlidersData';
+import leftArrow from 'assests/images/pagination/left-arrow.svg';
+import rightArrow from 'assests/images/pagination/right-arrow.svg';
 import classes from 'styles/sliders.module.scss';
 
 const Slider = () => {
-  const [current, setCurrent] = useState(1);
+  const [current, setCurrent] = useState(0);
+  if (!Array.isArray(SliderData) || SliderData.length <= 0) {
+    return null;
+  }
+  const changeSlide = (current: any) => {
+    if (current === SliderData.length) {
+      setCurrent(0);
+      return;
+    }
+    if (current < 0) {
+      setCurrent(SliderData.length - 1);
+      return;
+    }
+    setCurrent(current);
+  };
+
+  const startTimer = () => {
+    setInterval(() => {
+      changeSlide(current + 1);
+    }, 2000);
+  };
+
   return (
     <div className={classes.sliders}>
       <SliderItem activeSlide={current} />
+      <img onClick={() => changeSlide(current - 1)} src={leftArrow} className={classes.btn_left} />
+      <img
+        onClick={() => changeSlide(current + 1)}
+        src={rightArrow}
+        className={classes.btn_right}
+      />
     </div>
   );
 };
--- task4/src/components/Slider/SliderItem.tsx
+++ task4/src/components/Slider/SliderItem.tsx
@@ -3,17 +3,17 @@ import { SliderData } from './SlidersData';
 
 const SliderItem = ({ activeSlide }: any) => {
   return (
-    <>
+    <div className={classes.sliders_items}>
       {SliderData.map((slide, index) => {
         return (
           <div
             key={index}
             className={index === activeSlide ? classes.slider__active : classes.slider}>
-            <img className={classes.item} src={slide.image} />
+            <img className={classes.slider__item} src={slide.image} />
           </div>
         );
       })}
-    </>
+    </div>
   );
 };
 
