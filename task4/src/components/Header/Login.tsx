682f32d20a410f9cf0d3789bea3d82e67669139b
Vadim Niziev <st1mpro23@yandex.ru>
2021-05-31 19:09:48 +0300

    переписаны асинхронные функции на asyc/await

--- task4/src/components/Header/Login.tsx
+++ task4/src/components/Header/Login.tsx
@@ -3,38 +3,60 @@ import { API_URL, API_KEY_STORE_FILM } from 'api/api';
 import classes from 'styles/header.module.scss';
 
 export default class Login extends React.Component {
-  sendPromises = () => {
-    fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_STORE_FILM}`)
-      .then((response) => response.json())
-      .then((data) =>
-        fetch(`${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_STORE_FILM}`, {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify({
-            username: 'Vadim2308',
-            password: 'PQD@SyK5wFeptKX',
-            request_token: data.request_token,
-          }),
-        })
-          .then((response: any) => response.json())
-          .then((data: any) =>
-            fetch(`${API_URL}/authentication/session/new?api_key=${API_KEY_STORE_FILM}`, {
-              method: 'POST',
-              mode: 'cors',
-              headers: {
-                'Content-type': 'application/json',
-              },
-              body: JSON.stringify({
-                request_token: data.request_token,
-              }),
-            })
-              .then((response) => response.json())
-              .then((data) => console.log(data)),
-          ),
-      );
+  sendPromises = async () => {
+    const fetchApi = (url: any, options = {}) => {
+      return new Promise((resolve, reject) => {
+        fetch(url, options)
+          .then((response) => {
+            if (response.status < 400) {
+              return response.json();
+            } else {
+              throw response;
+            }
+          })
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((response) => {
+            response.json().then((error: any) => {
+              reject(error);
+            });
+          });
+      });
+    };
+    const data: any = await fetchApi(
+      `${API_URL}/authentication/token/new?api_key=${API_KEY_STORE_FILM}`,
+    );
+
+    const result: any = await fetchApi(
+      `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_STORE_FILM}`,
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: 'Vadim2308',
+          password: 'PQD@SyK5wFeptKX',
+          request_token: data.request_token,
+        }),
+      },
+    );
+    const { session_id }: any = await fetchApi(
+      `${API_URL}/authentication/session/new?api_key=${API_KEY_STORE_FILM}`,
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          request_token: result.request_token,
+        }),
+      },
+    );
+    console.log(session_id);
   };
 
   render() {
