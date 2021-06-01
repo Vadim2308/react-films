a5888ffe3edebf9c50ccb2453815a2b183c4611c
Vadim Niziev <st1mpro23@yandex.ru>
2021-06-02 00:02:29 +0300

    Прикрутил к форме авторизации запрос, и ошибку при неверном л/п

--- task4/src/components/Header/Login.tsx
+++ task4/src/components/Header/Login.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import LoginForm from 'components/Header/LoginForm';
-import { API_URL, API_KEY_STORE_FILM } from 'api/api';
 // import { Modal, ModalBody } from 'reactstrap';
 import Modal from 'react-modal';
 import classes from 'styles/header.module.scss';
@@ -13,7 +12,7 @@ export default class Login extends React.Component<{}, IState> {
   constructor(props: any) {
     super(props);
     this.state = {
-      modal: true,
+      modal: false,
     };
   }
 
@@ -23,62 +22,6 @@ export default class Login extends React.Component<{}, IState> {
         modal: !prevState.modal,
       };
     });
-  };
-
-  sendPromises = async () => {
-    const fetchApi = (url: any, options = {}) => {
-      return new Promise((resolve, reject) => {
-        fetch(url, options)
-          .then((response) => {
-            if (response.status < 400) {
-              return response.json();
-            } else {
-              throw response;
-            }
-          })
-          .then((data) => {
-            resolve(data);
-          })
-          .catch((response) => {
-            response.json().then((error: any) => {
-              reject(error);
-            });
-          });
-      });
-    };
-    const data: any = await fetchApi(
-      `${API_URL}/authentication/token/new?api_key=${API_KEY_STORE_FILM}`,
-    );
-
-    const result: any = await fetchApi(
-      `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_STORE_FILM}`,
-      {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: 'Vadim2308',
-          password: 'PQD@SyK5wFeptKX',
-          request_token: data.request_token,
-        }),
-      },
-    );
-    const { session_id }: any = await fetchApi(
-      `${API_URL}/authentication/session/new?api_key=${API_KEY_STORE_FILM}`,
-      {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          request_token: result.request_token,
-        }),
-      },
-    );
-    console.log(session_id);
   };
 
   render() {
--- task4/src/components/Header/LoginForm.tsx
+++ task4/src/components/Header/LoginForm.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { API_URL, API_KEY_STORE_FILM } from 'api/api';
 import classes from 'styles/header.module.scss';
 
 interface IState {
   username: string;
   password: string;
   errors: {
-    username: string;
-    password: string;
+    username?: string;
+    password?: string;
+    invalid?: string;
   };
 }
 
@@ -16,12 +18,72 @@ export default class LoginForm extends React.Component<{}, IState> {
     this.state = {
       username: '',
       password: '',
-      errors: {
-        username: '',
-        password: '',
-      },
+      errors: {},
     };
   }
+
+  LoginUser = async () => {
+    const fetchApi = (url: any, options = {}) => {
+      return new Promise((resolve, reject) => {
+        fetch(url, options)
+          .then((response) => {
+            if (response.status < 400) {
+              return response.json();
+            } else {
+              throw response;
+            }
+          })
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((response) => {
+            response.json().then(() => {
+              this.setState((prevState) => {
+                return {
+                  ...prevState,
+                  errors: {
+                    invalid: 'Неверный логин и/или пароль.',
+                  },
+                };
+              });
+            });
+          });
+      });
+    };
+    const data: any = await fetchApi(
+      `${API_URL}/authentication/token/new?api_key=${API_KEY_STORE_FILM}`,
+    );
+
+    const result: any = await fetchApi(
+      `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_STORE_FILM}`,
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: this.state.username,
+          password: this.state.password,
+          request_token: data.request_token,
+        }),
+      },
+    );
+    const { session_id }: any = await fetchApi(
+      `${API_URL}/authentication/session/new?api_key=${API_KEY_STORE_FILM}`,
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          request_token: result.request_token,
+        }),
+      },
+    );
+    console.log(session_id);
+  };
 
   changeInput = (event: any) => {
     const name = event.target.name;
@@ -30,10 +92,7 @@ export default class LoginForm extends React.Component<{}, IState> {
       return {
         ...prevState,
         [name]: value,
-        errors: {
-          username: '',
-          password: '',
-        },
+        errors: {},
       };
     });
   };
@@ -43,16 +102,20 @@ export default class LoginForm extends React.Component<{}, IState> {
     if (this.state.username === '') {
       errors.username = 'Слишком короткое имя';
     }
-    if (this.state.username === '') {
+    if (this.state.password === '') {
       errors.password = 'Слишком короткий пароль';
     }
-    console.log('errors', errors);
     return errors;
   };
 
   handleLogin = (event: any) => {
     event.preventDefault();
     const errors = this.validatingInputs();
+    if (errors.username === '' && errors.password === '') {
+      this.LoginUser();
+    } else {
+      return;
+    }
   };
 
   onBlure = () => {
@@ -68,8 +131,8 @@ export default class LoginForm extends React.Component<{}, IState> {
   };
 
   render() {
-    console.log(this.state);
     const { username, password, errors } = this.state;
+    console.log(errors);
     return (
       <div className={classes.modal}>
         <form className={classes.login_form}>
@@ -108,6 +171,7 @@ export default class LoginForm extends React.Component<{}, IState> {
             type="submit">
             Войти
           </button>
+          {errors.invalid && <div className={classes.warning__user}>{errors.invalid}</div>}
         </form>
       </div>
     );
